f39bd8f1d7e365169dd317bf9f11a250c1b4e3be
Nolan Kosmal <nkosm002@xe-10.cs.ucr.edu>
2025-11-18 16:37:01 -0800

    added more tables

--- create-table.sql
+++ create-table.sql
@@ -2,25 +2,71 @@ DROP TABLE IF EXISTS ... CASCADE;
 
 
 CREATE TABLE HOTEL (
-    int hotelid PRIMARY KEY,
-    TEXT name,
-    TEXT address,
+    hotelID INT PRIMARY KEY,
+    name TEXT,
+    address TEXT
 
 );
 
 -- one (hotel) to many (staff) relationship 
 CREATE TABLE STAFF (
-    int SSN PRIMARY KEY, -- is this a primary key (?) doesn't show up on the ER diagram
-    TEXT fname,
-    TEXT lname,
-    TEXT position,
-    TEXT address
-    
-    int hotelid
-    FOREIGN KEY (hotelid) REFERENCES HOTEL(hotelid) ON DELETE CASCADE
+    SSN INT PRIMARY KEY, -- is this a primary key (?) doesn't show up on the ER diagram
+    fname TEXT,
+    lname TEXT,
+    position TEXT
+    address TEXT
+    int hotelID
+    FOREIGN KEY (hotelID) REFERENCES HOTEL(hotelID) ON DELETE CASCADE
 );
 
 CREATE TABLE ROOM (
+    roomNo INT,
+    roomType TEXT,
 
+    hotelID INT NOT NULL,
+    PRIMARY KEY (roomNo, hotelId),
+    FOREIGN KEY (hotelId) REFERENCES HOTEL(hotelID) ON DELETE CASCADE,
+
+    companyID INT NOT NULL, -- room must be repaired by one maintenance company
+
+);
+
+CREATE TABLE REPAIR (
+    repairID INT PRIMARY KEY,
+    roomID INT NOT NULL,
+    repairType TEXT,
+    repairDate DATE
+    description TEXT,
+    FOREIGN KEY (roomID) REFERENCES Room(roomID) -- each repair matches to one room, which in turn matches to one company
+);
+
+CREATE TABLE CUSTOMER (
+    customerID INT PRIMARY KEY,
+    fname TEXT,
+    lname TEXT,
+    gender TEXT,
+    address TEXT,
+    phNo TEXT,
+    DOB DATE,
+);
+
+-- many (customer) to many (room) relationship junction table
+CREATE TABLE BOOKING (
+    bookingDate DATE,
+    numPeople INT,
+    price INT,
+
+    customerID INT,
+    roomID INT,
+    PRIMARY KEY (customerID, roomID),
+    FOREIGN KEY (customerID) REFERENCES CUSTOMER(customerID) ON DELETE CASCADE,
+    FOREIGN KEY (roomID) REFERENCES ROOM(roomNo) ON DELETE CASCADE
+);
+
+CREATE TABLE MAINTENANCE_COMPANY (
+    companyID INT PRIMARY KEY,
+    name TEXT, 
+    address TEXT,
+    certified BOOLEAN,
 
 );
