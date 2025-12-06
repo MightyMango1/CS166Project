8820d16f8f51947e71d6262c518a3ba19518f46b
mchav138 <146979886+mchav138@users.noreply.github.com>
2025-12-05 18:55:14 -0800

    previously wrong file, new correct file for indexes
    Updated comments for clarity on index creation.

--- sql/indexes.sql
+++ sql/indexes.sql
@@ -1,18 +1,17 @@
 
---customer by name lookup index
--- Fast lookup by first and last name queries 11 and 12
+--customer by name lookup index queries 11 and 12
 CREATE INDEX idx_customer_fname_lname 
 ON Customer(fName, lName);
 
--- Filter by maintenance company (query 14)
+--maintenance company id lookup query 14
 CREATE INDEX idx_repair_mcompany 
 ON Repair(mCompany);
 
--- Filter by hotel and room (query 16)
+--hotela and room filter query 16
 CREATE INDEX idx_repair_hotel_room_date 
 ON Repair(hotelID, roomNo, repairDate);
 
--- Lookup by name (query 14)
+-- Lookup mcomp name query 14
 CREATE INDEX idx_maintenancecompany_name 
 ON MaintenanceCompany(name);
 
